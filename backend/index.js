cf61b08f6a5b10bf7727e8ff4b9292ee9bdec58e
huygamcha <lehuynhhuy2002@gmail.com>
2023-12-27 11:28:33 +0700

    up

--- backend/index.js
+++ backend/index.js
@@ -44,7 +44,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://pbl3-chatbot.vercel.app",
     // credentials: true,
   },
 });
