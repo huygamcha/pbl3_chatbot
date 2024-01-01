4d87f5304891d216a2344c0706087b9bd2265b3f
huygamcha <lehuynhhuy2002@gmail.com>
2024-01-02 00:18:27 +0700

    up

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -16,7 +16,7 @@ async function forgotPassword(req, res) {
   const user = await User.findOne({ email: req.body.email });
 
   if (!user) {
-    return res.status(404).json({ error: "User not found" });
+    return res.status(404).json({ message: "User not found" });
   }
 
   // Generate a password reset token and set its expiration date
