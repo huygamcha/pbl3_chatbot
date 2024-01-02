2384f03fdcddfbab42a11976f6db1772503c80f8
huygamcha <lehuynhhuy2002@gmail.com>
2024-01-02 14:02:46 +0700

    up

--- backend/index.js
+++ backend/index.js
@@ -49,5 +49,5 @@ const io = require("socket.io")(server, {
   },
 });
 io.on("connection", (socket) => {
-  console.log("Connected to socket.io", 123);
+  console.log("Connected to socket.io", socket);
 });
